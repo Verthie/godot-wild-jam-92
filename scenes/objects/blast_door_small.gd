5187731714632f83c82d8fdb41cab19a6b254e3b
Verthie <vertisixs@gmail.com>
2026-04-16 18:30:36 +0200

    fix: door is no longer open from the beginnning

--- scenes/objects/blast_door_small.gd
+++ scenes/objects/blast_door_small.gd
@@ -9,10 +9,14 @@ func _ready() -> void:
 	player_detector.body_exited.connect(_on_body_exited)
 
 
-func _on_body_entered(_body: Node3D) -> void:
+func _on_body_entered(body: Node3D) -> void:
+	if body is not Player:
+		return
 	AudioManager.create_3d_audio_at_location(global_position, SoundEffect.SoundEffectType.DOOR_OPEN)
 	animation_player.play("Door_move")
 
-func _on_body_exited(_body: Node3D) -> void:
+func _on_body_exited(body: Node3D) -> void:
+	if body is not Player:
+		return
 	AudioManager.create_3d_audio_at_location(global_position, SoundEffect.SoundEffectType.DOOR_CLOSE)
 	animation_player.play_backwards("Door_move")
